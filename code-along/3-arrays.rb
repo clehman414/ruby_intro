c76259b8f6e5bcccf8324f9d58170d28aecb990b
Caitlin Lehman <caitlin.lehman@kellogg.northwestern.edu>
2025-01-28 21:24:00 +0000

    complete labs 1-4

--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -4,9 +4,42 @@
 
 # Arrays are lists of things
 
+favorite_foods = ["tacos", "pizza", "ice cream"]
+puts favorite_foods
+
+p favorite_foods
+
+we_have_to_go_back = [4, 8, 15, 16, 23, 42]
+puts we_have_to_go_back
+p we_have_to_go_back
+
+mixed_array = ["tacos", 12, true]
+p mixed_array
+p mixed_array.length
+
 # Accessing the array
 
+puts mixed_array[0]
+puts mixed_array[-1]
+
+shopping_lists = [["milk", "eggs", "toilet paper"], ["soap", "shampoo", "shaving cream", "toilet paper"]]
+p shopping_lists
+
+p shopping_lists [0][1]
+p shopping_lists [0][2]
+
 # Add to the array
+
+favorite_foods << "more tacos"
+puts favorite_foods
+
+favorite_foods = favorite_foods + ["french fries", "ramen"]
+puts favorite_foods
+
+puts favorite_foods.count
+puts favorite_foods.size
+puts favorite_foods.length
+
 
 # There are lots of fun things you can do with arrays:
 # https://ruby-doc.org/core-2.7.0/Array.html
--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,35 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+ben = { "name" => "Ben",
+"location" => "Chicago",
+"status" => "ENTR-924 at Kellogg!"}
+
+puts ben 
+p ben 
+
 
 # Accessing data from the hash
+location = ben["location"]
+puts location
 
 # More Complex Hashes
+
+my_profile = {
+    "name" => "Ben",
+    "location" => {"city" => "Chicago", "state" => "Illinois"},
+    "status" => "ENTR-924 at Kellogg"
+}
+puts my_profile
+
+puts my_profile["location"]["city"]
+
+my_profile["name"] = "Nathan Jones"
+puts my_profile
+
+complete_profile = {"name" => "Ben Block",
+"location" => {"city" => "Chicago", "state" => "Illinois"},
+"timeline" => [{"status" => "Eating tacos", "posted" => "7:30am"},
+{"status" => "Brushing teeth", "posted" => "8:00am"},
+{"status" => "Eating mode tacos", "posted" => "7:30am"}]}
+
